0bcabfe0ca8945617b4f19c028f1e51743582604
landrea-velez <landrea.velez@gmail.com>
2025-06-11 17:03:28 -0500

    base del proyecto REST

---
+++ src/main/java/com/landreavelez/springboot/webflux/app/SpringBootWebfluxApirestApplication.java
@@ -0,0 +1,13 @@
+package com.landreavelez.springboot.webflux.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxApirestApplication.class, args);
+	}
+
+}
