845aa5263c807c7ccdc5c5fbace37f4efb838027
landrea-velez <landrea.velez@gmail.com>
2025-06-12 13:59:06 -0500

    commit correciones en repositorio

---
+++ src/main/java/com/landreavelez/springboot/webflux/app/controllers/ProductoController.java
@@ -0,0 +1,10 @@
+package com.landreavelez.springboot.webflux.app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoController {
+
+}
