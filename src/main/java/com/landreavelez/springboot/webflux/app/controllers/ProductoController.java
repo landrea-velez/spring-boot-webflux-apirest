58093ea448cbe2769f4c284ac42ac741ff23b386
landrea-velez <landrea.velez@gmail.com>
2025-06-12 14:07:28 -0500

    GET para listar con rest controller

--- src/main/java/com/landreavelez/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/landreavelez/springboot/webflux/app/controllers/ProductoController.java
@@ -1,10 +1,32 @@
 package com.landreavelez.springboot.webflux.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.landreavelez.springboot.webflux.app.models.documents.Producto;
+import com.landreavelez.springboot.webflux.app.models.services.ProductoService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/productos")
 public class ProductoController {
+	
+	@Autowired
+	private ProductoService service;
+	
+	@GetMapping
+	public Mono<ResponseEntity<Flux<Producto>>> lista(){
+		return Mono.just(
+				ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.body(service.findAll())
+				);
+	}
 
 }
