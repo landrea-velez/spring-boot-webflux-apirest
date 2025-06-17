e63d8e518e2797db8002ffce6c89e74ea3b911e3
landrea-velez <landrea.velez@gmail.com>
2025-06-17 14:16:11 -0500

    Test GET listar

--- src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,16 +1,46 @@
 package com.bolsadeideas.springboot.webflux.app;
 
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.landreavelez.springboot.webflux.app.models.documents.Producto;
+
+import org.springframework.http.MediaType;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBootWebfluxApirestApplicationTests {
+	
+	@Autowired
+	private WebTestClient client;
 
 	@Test
-	public void contextLoads() {
+	public void listarTest() {
+		
+		client.get()
+		.uri("/api/v2/productos")
+		.accept(MediaType.APPLICATION_JSON_UTF8)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+		.expectBodyList(Producto.class)
+		.consumeWith(response -> {
+			List<Producto> productos = response.getResponseBody();
+			productos.forEach(p -> {
+				System.out.println(p.getNombre());
+			});
+			
+			Assertions.assertThat(productos.size()>0).isTrue();
+		});
+		//.hasSize(9);
 	}
 
 }
